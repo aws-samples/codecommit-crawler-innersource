be3d2ca159b5a09cbf5b77af0524c6752e0105c6
Debashish <debashish@gmail.com>
2021-08-12 18:26:46 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-## My Project
+# AWS CodeCommit Crawler for InnerSource Portal
+Organizations setting up an [InnerSource](https://innersourcecommons.org/) ecosystem in their intranet should be able to use any [Source Code Control system](https://en.wikipedia.org/wiki/Source_Code_Control_System). This project assists in setting up a crawler for [AWS CodeCommit](https://aws.amazon.com/codecommit/) based InnerSource code repositories that can be utilized by the [SAP InnerSource Portal](https://github.com/sap/project-portal-for-InnerSource). The crawler can made to fetch these details automatically every once a while using [cron](https://en.wikipedia.org/wiki/Cron) construct. [Click here](https://github.com/SAP/project-portal-for-innersource/blob/main/docs/CRAWLING.md) to know more about the Crawler.
 
-TODO: Fill this README out!
+The project creates a `repos.json` to be consumed by the [SAP InnerSource Portal](https://github.com/sap/project-portal-for-InnerSource) to display available InnerSource projects. The solution assumes that you have the CodeCommit repositories already setup and that the crawler is able to connect to them using AWS credentials (namely, *aws_access_key_id* and *aws_secret_access_key*). 
 
-Be sure to:
+The crawler implements a custom logic for assigning the  [activity score](https://patterns.innersourcecommons.org/p/repository-activity-score) and omits the fields that are not available/relevant for CodeCommit (e.g. *Fork* or *Star*).
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## Installation
+
+    pip install -r requirements.txt
+
+## Usage
+
+1. (Optional) Add a tag to your InnerSource repos with key as `type` and value as `innersource`
+2. (Optional) Add an `innersource.json` file in each repo (a sample file is included in this repo), with the details about the project.
+3. Run `python3 ./crawler.py`, which will create a `repos.json` file containing the relevant metadata for the AWS CodeCommit repos
+4. Copy `repos.json` to your instance of the SAP InnerSource Portal and launch the portal as outlined in their installation instructions.
+
+## Customization
+While the entire code can be customized according to your use case, a particular customization might be needed if your AWS CodeCommit installation contains repositories other than the InnerSource repos. In such a case you may want to filter out the InnerSource ones using tags, such as `type = innersource`. An example code to implement this filter is provided:
+
+    tag_data = cc_client.list_tags_for_resource(
+    	resourceArn = repo_metadata["Arn"]
+    )
+    repo_tags = tag_data["tags"]
+    repoType = repo_tags["type"]
+    if repoType != "innersource":
+    	break
+
+![CodeCommit Crawler](innersource_crawler_codecommit.png)
+
+Similarly, you may chose to add an `innersource.json` file in each of your InnerSource repo (a sample file is included in this repo), with the details about the project. This helps in populating the fields on the portal information of which cannot be fetched from CodeCommit.
+
 
 ## Security
 
@@ -14,4 +39,3 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
-
---
+++ crawler.py
@@ -0,0 +1,158 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+#!/usr/bin/env python
+import boto3
+import json
+import os
+import io
+from datetime import datetime, timezone
+import math
+from base64 import b64decode
+from os.path import dirname, join
+
+# calculate a virtual InnerSource score from stars, watches, commits, and issues
+def calculateScore(repoData):
+    # initial score is 50 to give active repos with low KPIs (forks, watchers, stars) a better starting point
+    iScore = 50;
+    # weighting: forks and watches count most, then stars, add some little score for open issues, too
+    iScore += repoData["forks_count"] * 5;
+    
+    #Caculate days elapsed since the repo was created and last updated
+    repoUpdateDate = repoData['updated_at']
+    repoCreateDate = repoData['created_at']
+    currentDate = datetime.now(timezone.utc)
+    iDaysSinceLastUpdate = currentDate - repoUpdateDate;
+    iDaysSinceCreation =  currentDate - repoCreateDate;
+
+    # updated in last 3 months: adds a bonus multiplier between 0..1 to overall score (1 = updated today, 0 = updated more than 100 days ago)
+    iScore = iScore * (1 + (100 - min(iDaysSinceLastUpdate.days, 100)) / 100);
+    
+    # evaluate participation stats 
+    # average commits: adds a bonus multiplier between 0..1 to overall score (1 = >10 commits per week, 0 = less than 3 commits per week)
+    #iAverageCommitsPerWeek = repo._InnerSourceMetadata.participation.slice(repo._InnerSourceMetadata.participation - 13).reduce((a, b) => a + b) / 13;
+    #iScore = iScore * (1 + (min(max(repoData['participation'] - 3, 0), 7)) / 7);
+    
+    # boost calculation:
+    # all repositories updated in the previous year will receive a boost of maximum 1000 declining by days since last update
+    iBoost = (1000 - min(iDaysSinceLastUpdate.days, 365) * 2.74);
+    
+    # gradually scale down boost according to repository creation date to mix with "real" engagement stats
+    iBoost *= (365 - min(iDaysSinceCreation.days, 365)) / 365;
+    # add boost to score
+    iScore += iBoost;
+    
+    # give projects with a meaningful description a static boost of 50
+    iScore += 50 if len(repoData['description']) > 200 else 0;
+    
+    # give projects with contribution guidelines (CONTRIBUTING.md) file a static boost of 100
+    if("_InnerSourceMetadata" in repoData and "contributions" in repoData["_InnerSourceMetadata"]):
+        iScore += 100 if len(repoData["_InnerSourceMetadata"]["contributions"]) > 1 else 0;
+    
+    # build in a logarithmic scale for very active projects (open ended but stabilizing around 5000)
+    if (iScore > 3000):
+        iScore = 3000 + math.log(iScore) * 100;
+    
+    # final score is a rounded value starting from 0 (subtract the initial value)
+    iScore = round(iScore - 50);
+
+    return iScore;
+
+#def lambda_handler(event, context):
+if __name__ == "__main__":
+
+    finalData = []
+    ccClient = boto3.client('codecommit')
+    
+    #Get alist of existing repositories
+    list_repo_response = ccClient.list_repositories(
+        sortBy = 'repositoryName',
+        order = 'ascending'
+    )    
+    repos = list_repo_response["repositories"]
+    
+    #Iterate over the list of repos
+    for repo in repos:
+        #this dict will hold each repo related data
+        repoData = {}
+        
+        #Get the repo name
+        repoName = repo["repositoryName"]
+        #Get repo metadata
+        repoDetails = ccClient.get_repository(
+            repositoryName = repoName
+        )
+        repoMetadata = repoDetails["repositoryMetadata"]
+        
+        #If your CodeCommit installation contains respositories other than the InnerSource repos
+        #you may want to filter out the InnerSource ones using tags, such as "type = innersource"
+        #Fetch additional info from tags
+        tagData = ccClient.list_tags_for_resource(
+            resourceArn = repoMetadata["Arn"]
+        )
+        repoTags = tagData["tags"]
+        if "type" in repoTags:
+            repoType = repoTags["type"]
+            if repoType != "innersource":
+                continue
+        else:
+            continue
+
+        #Populate our dict from metadata
+        repoData['id'] = repoMetadata["repositoryId"]
+        repoData['name'] = repoMetadata["repositoryName"]
+        repoData['full_name'] = repoMetadata["repositoryName"]
+        repoData['html_url'] = repoMetadata["cloneUrlHttp"]
+        if "repositoryDescription" in repoMetadata:
+            repoData['description'] = repoMetadata["repositoryDescription"]
+        repoData['created_at'] = repoMetadata["creationDate"]
+        repoData['updated_at'] = repoMetadata["lastModifiedDate"]
+        repoData['pushed_at'] = repoMetadata["lastModifiedDate"]
+        if("defaultBranch" in repoMetadata):
+            repoData['default_branch'] = repoMetadata["defaultBranch"]    
+
+        #Get number of branches to mimic fork count
+        listOfBranches = ccClient.list_branches(
+                repositoryName = repoName
+            )
+        repoData['forks_count'] =  len(listOfBranches["branches"])
+        repoData['_InnerSourceMetadata'] = {}
+      
+        #If the repository contains an innersource.json, read the file and add to our JSON
+        try:            
+            innerSourceJsonFile = ccClient.get_file(
+                repositoryName = repoName,
+                filePath = "innersource.json"
+            )
+
+            innerSourceJsonContent = innerSourceJsonFile["fileContent"]
+            innerSourceDecoded = innerSourceJsonContent.decode('utf-8')
+            innerSourceJson = json.loads(innerSourceDecoded)
+            repoData['_InnerSourceMetadata'] = innerSourceJson
+            repoData['language'] = innerSourceJson['language']
+            repoData['license'] = innerSourceJson["license"]    
+            repoData['topics'] = innerSourceJson["topics"]  
+            repoData['_InnerSourceMetadata']['score'] = calculateScore(repoData)  
+
+        except (ccClient.exceptions.FileDoesNotExistException, ccClient.exceptions.CommitDoesNotExistException) as e:
+            print("Looks like there is no innersource.json in the repo ", repoName, ". You may like to add one.")
+        finally:  
+            #No CodeCommit equivalents, you may chose to add it via innersource.json
+            repoData['stargazers_count'] = ''
+            repoData['watchers_count'] = ''        
+            repoData['open_issues_count'] = ''  
+            repoData['score'] = calculateScore(repoData)
+            
+            #Without owner the portal breaks, so we give some dummy values
+            repoData['owner'] = {}
+            repoData['owner']["login"] = "Noble"
+            repoData['owner']["avatar_url"] = "./images/demo/Sol.png"   
+        
+        
+        finalData.append(repoData)         
+    
+    
+    #write the content in a json file
+    with open("repos.json", "w") as f:
+        json.dump(finalData, f, indent=4,sort_keys=False,default=str)
+        print("repos.json file written")
